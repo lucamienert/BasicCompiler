7ca7b3f4b02324bb3a3c909b61d44f5d1fbea485
Luca Mienert <67235946+lucamienert@users.noreply.github.com>
2022-07-25 16:27:51 +0200

    Added Parser

---
+++ BasicCompiler/Parsing/Parser.cs
@@ -0,0 +1,37 @@
+﻿using BasicCompiler.Exceptions;
+using BasicCompiler.Token;
+
+namespace BasicCompiler.Parsing;
+
+public class Parser : IDisposable
+{
+    private readonly Tokenizer _tokenizer;
+    private Token.Token _token;
+
+    public Parser(Tokenizer tokenizer)
+    {
+        _tokenizer = tokenizer;
+        _token = tokenizer.NextToken();
+    }
+
+    private void Consume() => _token = _tokenizer.NextToken();
+
+    private bool Accept(TokenType type)
+    {
+        if(_token.Type != type)
+            return false;
+
+        Consume();
+        return true;
+    }
+
+    private void Expect(TokenType type)
+    {
+        if (!Accept(type))
+            throw new UnexpectedTokenException(type);
+    }
+
+    public void Dispose()
+    {
+    }
+}
